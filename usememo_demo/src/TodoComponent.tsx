276a2c5105c2c0cec6d56b5362dc2635bd9cad0b
Sudipto Saha <sahasudipto336@gmail.com>
2024-02-10 06:33:00 +0530

    Update Todo component

--- usememo_demo/src/TodoComponent.tsx
+++ usememo_demo/src/TodoComponent.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 
 function TodoComponent() {
-  return <div>Todo Component</div>;
+  const [todos, setTodos] = useState<string[]>([]);
+  const [count, setCount] = useState<number>(0);
+
+  //const expensiveOperationResult = expensiveOperation();
+
+  const expensiveOperationResult = useMemo(() => expensiveOperation(), []);
+
+  const addTodo = () => {
+    setTodos((previousTodos) => [...previousTodos, "New Todo"]);
+  };
+
+  const incrementCount = () => {
+    setCount((previousCount) => previousCount + 1);
+  };
+
+  const decrementCount = () => {
+    setCount((previousCount) => previousCount - 1);
+  };
+
+  //without
+  return (
+    <div>
+      <div>
+        <h1>Todos</h1>
+        {todos.map((todo, index) => (
+          <p key={index}>{todo}</p>
+        ))}
+        <br />
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+      <br />
+      <div>
+        <p>Count: {count}</p>
+        <button onClick={incrementCount}>+</button>
+        <button onClick={decrementCount}>-</button>
+      </div>
+      <br />
+      <p>Expensive operation result: {expensiveOperationResult}</p>
+    </div>
+  );
 }
+
+const expensiveOperation = () => {
+  let count = 0;
+  for (let i = 1; i <= 1000000000000000; i++) {
+    count += i;
+  }
+  return count;
+};
 
 export default TodoComponent;
